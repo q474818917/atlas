727e427f0ef1ef62c1c96e17e580bb9fc08bb3bd
q474818917 <q474818917@163.com>
2015-12-08 16:07:34 +0800

    add ESAction instance

--- src/main/java/com/dwarf/utils/ESAction.java
+++ src/main/java/com/dwarf/utils/ESAction.java
@@ -1,21 +1,85 @@
 package com.dwarf.utils;
 
-import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Properties;
+
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.node.Node;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ESAction {
+	
+	private static Logger logger = LoggerFactory.getLogger(ESAction.class);
+	
+	private static ESAction _ESAction;
+	
+	private static Properties prop = new Properties();
+	
+	static {
+		InputStream in = ESAction.class.getClassLoader().getResourceAsStream("server.properties");
+		try {
+			prop.load(in);
+			logger.info("server.properties loading succeed");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Client client;
+	
+	private ESAction(){
+		Settings settings = Settings.settingsBuilder()
+		        .put("cluster.name", prop.getProperty("es.clusterName")).build();
+		
+		TransportClient tsClient = TransportClient.builder().settings(settings).build();
+		for(int i = 0; i < prop.getProperty("es.server").split(",").length; i++){
+			try {
+				tsClient.addTransportAddress(new InetSocketTransportAddress(
+						InetAddress.getByName(prop.getProperty("es.server").split(",")[i]), Integer.parseInt(prop.getProperty("es.port").split(",")[i])));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		client = tsClient;
+	}
+	
+	public static ESAction getInstance(){
+		if(_ESAction == null){
+			_ESAction = new ESAction();
+		}
+		return _ESAction;
+	}
 
-	public static void main(String[] args) {
-		Node node = nodeBuilder().clusterName("clusterES").node();
-		Client client = node.client();
+	public static void main(String[] args) throws IOException {
+		
+		IndexResponse response = ESAction.getInstance().client.prepareIndex("twitter", "tweet", "1")
+		        .setSource(jsonBuilder()
+		                    .startObject()
+		                        .field("user", "kimchy")
+		                        .field("postDate", new Date())
+		                        .field("message", "trying out Elasticsearch")
+		                    .endObject()
+		                  )
+		        .get();
+		
 		try {
 			Thread.sleep(14400);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		node.close();
 	}
 
 }
