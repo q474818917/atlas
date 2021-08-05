d36dc6cb5108d4fc0c49d56b0b6cd3a5aa9096ea
dwarf <q474818917@163.com>
2021-08-05 15:21:52 +0800

    add appium

--- src/main/java/com/taobo/MainActivity.java
+++ src/main/java/com/taobo/MainActivity.java
@@ -4,6 +4,8 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.apdplat.word.WordFrequencyStatistics;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -27,11 +29,20 @@ public class MainActivity {
             AppiumDriver<MobileElement> driver = new AppiumDriver<>(
                     new URL("http://127.0.0.1:4723/wd/hub"),
                     capabilities);
+            //根据ID获取页面元素
            MobileElement element =  driver.findElementById("");
+           //页面刷新
+           driver.navigate().refresh();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
+        /*WordFrequencyStatistics wordFrequencyStatistics = new WordFrequencyStatistics();
+        wordFrequencyStatistics.setRemoveStopWord(false);
+        wordFrequencyStatistics.setResultPath("word-frequency-statistics.txt");
+        wordFrequencyStatistics.setSegmentationAlgorithm(SegmentationAlgorithm.MaxNgramScore);
+        //开始分词
+        wordFrequencyStatistics.seg("阿根廷");
+        wordFrequencyStatistics.dump();*/
     }
 }
